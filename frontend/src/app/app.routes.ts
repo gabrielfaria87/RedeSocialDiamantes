ac4190bc6e8047ef19957708816310f0f27af725
luistn <luiis.torres@hotmail.com>
2025-09-01 15:50:50 -0300

    galeria e home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,14 +6,18 @@ import { ChatComponent } from './componentes/chat/chat.component';
 import { PerfilComponent } from './componentes/perfil/perfil.component';
 import { AdminComponent } from './componentes/admin/admin.component';
 import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './componentes/home/home.component';
+import { GaleriaComponent } from './componentes/galeria/galeria.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'galeria', component: GaleriaComponent, canActivate: [AuthGuard] },
   { path: 'feed', component: FeedComponent, canActivate: [AuthGuard] },
   { path: 'forum', component: ForumComponent, canActivate: [AuthGuard] },
   { path: 'chat', component: ChatComponent, canActivate: [AuthGuard] },
   { path: 'perfil', component: PerfilComponent, canActivate: [AuthGuard] },
   { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/feed', pathMatch: 'full' },
-  { path: '**', redirectTo: '/feed' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
 ];
